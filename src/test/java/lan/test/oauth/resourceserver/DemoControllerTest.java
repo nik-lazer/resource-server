23b06976cedc14b6a0980699e0930bc6a1595457
nik-lazer <nick.lazarev@gmail.com>
2023-12-14 19:40:58 +0200

    remove unused import

--- src/test/java/lan/test/oauth/resourceserver/DemoControllerTest.java
+++ src/test/java/lan/test/oauth/resourceserver/DemoControllerTest.java
@@ -6,9 +6,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.equalToIgnoringCase;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
